6e954f41d6ac4ae6740d9625c2764d8fd8cb820d
g1.kwon <spring_shot@naver.com>
2020-02-08 21:35:52 +0900

    add Naver OAuth2 Login

--- src/main/java/com/jojoldu/book/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/jojoldu/book/springboot/config/auth/dto/OAuthAttributes.java
@@ -26,6 +26,11 @@ public class OAuthAttributes {
 
     //of() : OAuth2User에서 반환하는 사용자 정보는 Map이기에 값 하나하나를 변환해야 한다.
     public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+
+        if ("naver".equals(registrationId)) {
+            return ofNaver("id", attributes);
+        }
+
         return ofGoogle(userNameAttributeName, attributes);
     }
 
@@ -39,7 +44,7 @@ public class OAuthAttributes {
                 .build();
     }
 
-    private static OAuthAttributes of(String userNameAttributeName, Map<String, Object> attributes) {
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
         Map<String, Object> response = (Map<String, Object>) attributes.get("response");
 
         return OAuthAttributes.builder()
